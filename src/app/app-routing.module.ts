f3cc78e27dd94f964c3d2f67385771103ca3b87a
580dev <dansteve580@gmail.com>
2023-07-09 19:19:56 +0200

    new home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeModule } from './home/home.module';
 
 const routes: Routes = [
-  {path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)}
+  
 ];
 
 @NgModule({
